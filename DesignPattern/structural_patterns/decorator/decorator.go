df4aad6fe3a3085ac54a2d9748770d41584fc5fb
xtzjsgfw <202741256@qq.com>
2020-11-02 23:20:38 +0800

    装饰器模式

---
+++ DesignPattern/structural_patterns/decorator/decorator.go
@@ -0,0 +1,40 @@
+package decorator
+
+import "fmt"
+
+type Component interface {
+	Describe() string
+	GetCount() int
+}
+
+type Fruit struct {
+	Count       int
+	Description string
+}
+
+func (f *Fruit) Describe() string {
+	return f.Description
+}
+
+func (f *Fruit) GetCount() int {
+	return f.Count
+}
+
+// 装饰器结构体
+type AppleDecorator struct {
+	Component
+	Type string
+	Num  int
+}
+
+func (apple *AppleDecorator) Describe() string {
+	return fmt.Sprintf("%s, %s", apple.Component.Describe(), apple.Type)
+}
+
+func (apple *AppleDecorator) GetCount() int {
+	return apple.Component.GetCount() + apple.Num
+}
+
+func CreateAppleDecorator(c Component, t string, n int) Component {
+	return &AppleDecorator{c, t, n}
+}
